c7d14bf443da5a16b4f11b878bcd468be85c4415
prystupa <eugene.prystupa@gmail.com>
2013-02-11 09:45:08 -0500

    - add stub for Nettoshpere web/push server

--- dashboard/src/main/scala/net/unified/DashboardApp.scala
+++ dashboard/src/main/scala/net/unified/DashboardApp.scala
@@ -1,6 +1,20 @@
 package net.unified
 
+import org.atmosphere.nettosphere.{Nettosphere, Config}
+import org.slf4j.LoggerFactory
+
 object DashboardApp extends App {
 
-  println("Hello World!")
+  private val logger = LoggerFactory.getLogger(getClass)
+
+  val configBuilder = new Config.Builder()
+  configBuilder.resource("./webapps")
+    .port(8080)
+    .host("127.0.0.1")
+    .build()
+
+  val nettosphere = new Nettosphere.Builder().config(configBuilder.build()).build()
+  nettosphere.start()
+
+  logger.info("Nettosphere Dashboard Server started on port {}", 8080)
 }
