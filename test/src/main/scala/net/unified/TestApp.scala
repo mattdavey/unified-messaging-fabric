ba25740e8041edac6b004efe96528ca017cc0f53
prystupa <eugene.prystupa@gmail.com>
2013-03-17 21:15:43 -0400

    - adding more feed handlers

--- test/src/main/scala/net/unified/TestApp.scala
+++ test/src/main/scala/net/unified/TestApp.scala
@@ -43,6 +43,9 @@ class TestApp {
     // Start pricing line handler simulator(s)
     val pricingHandler = injector.getInstance(classOf[PricingService])
     pricingHandler.start(LineHandlerConfig("360T", List("EUR/USD", "USD/JPY", "GBP/USD", "AUD/USD", "USD/CHF", "USD/CAD")))
+    pricingHandler.start(LineHandlerConfig("FXall", List("USD/TRY", "EUR/TRY", "USD/ZAR", "EUR/ZAR", "USD/CNY", "USD/INR", "USD/MXN", "USD/SGD")))
+    pricingHandler.start(LineHandlerConfig("Currenex", List("GBP/CAD", "GBP/CHF", "GBP/JPY", "GBP/NZD", "GBP/AUD", "CAD/JPY", "AUD/JPY", "AUD/CAD", "AUD/NZD", "AUD/CHF", "NZD/JPY", "CHF/JPY")))
+    pricingHandler.start(LineHandlerConfig("Bloomberg", List("EUR/USD", "USD/JPY", "GBP/USD", "AUD/USD", "USD/CHF", "USD/CAD", "EUR/CHF", "EUR/JPY", "EUR/GBP", "EUR/CAD", "EUR/AUD", "EUR/NZD")))
 
     println("[Enter] to exit...")
     readLine()
